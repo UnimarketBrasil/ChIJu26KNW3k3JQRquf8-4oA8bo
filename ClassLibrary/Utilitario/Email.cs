77d3aec48b0695e7a3cb6f0412486d1143ca7075
numero <numero@DESKTOP-L2LUH2J>
2017-03-18 11:10:16 -0300

    Classlibrary utilitario

---
+++ ClassLibrary/Utilitario/Email.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Utilitario
+{
+    class Email
+    {
+        public bool IsEmail(string email)
+        {
+
+            Regex rg = new Regex(@"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+
+            if (rg.IsMatch(email))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
