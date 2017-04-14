9f5436b42f296aabe611295752fd6cc27d41dbe7
Lucas Sotero <Lucassdeabreu@outlook.com>
2017-04-14 11:47:05 -0300

    Geo coding e criptografia

--- ClassLibrary/ClassUtilitario/GeoCodificacao.cs
+++ ClassLibrary/ClassUtilitario/GeoCodificacao.cs
@@ -10,16 +10,16 @@ namespace ClassUtilitario
 {
     public class GeoCodificacao
     {
-        public Usuario ObterCoordenadas(Usuario user, string endereco)
+        public Usuario ObterCoordenadas(Usuario user, string rua, string numero)
         {
             DataSet data = new DataSet();
 
             try
             {
-                string viacep = string.Format("https://viacep.com.br/ws/{0}/xml/", endereco);
+                string viacep = string.Format("https://viacep.com.br/ws/{0}/xml/", rua);
                 data.ReadXml(viacep);
                 string enderecoPorCep = data.Tables[0].Rows[0]["logradouro"].ToString().Trim();
-                string googleMaps = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key=AIzaSyDPNFOUPna4dnTRtQ806ST8G9Vj6WEK32Y&new_forward_geocoder=true&address={0}", enderecoPorCep);
+                string googleMaps = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key=AIzaSyDPNFOUPna4dnTRtQ806ST8G9Vj6WEK32Y&new_forward_geocoder=true&address={0},{1}", enderecoPorCep, numero);
                 data = new DataSet();
                 data.ReadXml(googleMaps);
                 user.Latitude = data.Tables["location"].Rows[0]["lat"].ToString();
@@ -32,7 +32,7 @@ namespace ClassUtilitario
             catch (Exception)
             {
 
-                string googleMaps = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key=AIzaSyDPNFOUPna4dnTRtQ806ST8G9Vj6WEK32Y&new_forward_geocoder=true&address={0}", endereco);
+                string googleMaps = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key=AIzaSyDPNFOUPna4dnTRtQ806ST8G9Vj6WEK32Y&new_forward_geocoder=true&address={0},{1}", rua, numero);
                 data = new DataSet();
                 data.ReadXml(googleMaps);
                 user.Latitude = data.Tables["location"].Rows[0]["lat"].ToString();
@@ -58,7 +58,7 @@ namespace ClassUtilitario
             }
             catch (Exception ex)
             {
-                throw new Exception("Erro ao cadastrar item pedido : " + ex.Message);
+                throw new;
             }
         }
     }
---
+++ ClassLibrary/WebApplication/Documentos/Criptografia.js
@@ -0,0 +1,21 @@
+﻿function Encripta(dados) {
+    var chave = "assbdFbdpdPdpfPdAAdpeoseslsQQEcDDldiVVkadiedkdkLLnm";
+    var mensx = "";
+    var j = 1;
+    for (var i = 0; i < dados.length; i++) {
+        mensx += ((((dados.substr(i, 1).charCodeAt(0)) + ((chave.substr(j, 1).charCodeAt(0))))).fromCharCode);
+        j++;
+    }
+    document.getElementById("1").value = mensx;
+}
+function Descripta(dados) {
+    var chave = "assbdFbdpdPdpfPdAAdpeoseslsQQEcDDldiVVkadiedkdkLLnm";
+    var mensx = "";
+    var j = 1;
+    for (var i = 0; i < dados.length; i++) {
+        mensx += ((((dados.substr(i, 1).charCodeAt(0)) - ((chave.substr(j, 1).charCodeAt(0))))).fromCharCode);
+        j++;
+    }
+    document.getElementById("1").value = mensx;
+}
+
