23b7a6b6f65bd3e34fb149d3abebc6ffca08be2a
José Filho <josefilhosilva44@gmail.com>
2017-04-30 00:19:40 -0300

    Alterações na procedure LoginUsuario; Indepedente do status a procedure deve retornar se existe, o C# vai validar se está bloqueado, pendênte, etc.

--- ClassLibrary/WebApplication/Documentos/Querys/Selects/Procedure LoginUsuario.sql
+++ ClassLibrary/WebApplication/Documentos/Querys/Selects/Procedure LoginUsuario.sql
@@ -12,11 +12,11 @@ begin
 	if exists (select Usuario.Id from Usuario where (Usuario.Email = @Email))
 	begin
 		select Usuario.Id as Id, Usuario.Nome, Usuario.Email, Usuario.CpfCnpj, Usuario.Latitude, Usuario.Longitude, Usuario.AreaAtuacao, Usuario.IdStatusUsuario, Usuario.IdTipoUsuario
-		from Usuario where (Usuario.Email = @Email) and (Usuario.Senha = @Senha) and (Usuario.IdStatusUsuario = 1)
+		from Usuario where (Usuario.Email = @Email) and (Usuario.Senha = @Senha)
 	end
 	else if exists (select SubUsuario.Id as IdUsuario from SubUsuario where (SubUsuario.Email = @Email))
 		select SubUsuario.IdUsuario as Id, SubUsuario.Nome, SubUsuario.Email, Usuario.CpfCnpj, Usuario.Latitude, Usuario.Longitude, Usuario.AreaAtuacao, Usuario.IdStatusUsuario, Usuario.IdTipoUsuario 
 		from SubUsuario 
 		inner join Usuario on (Usuario.ID = SubUsuario.IdUsuario)
-		where (SubUsuario.Email = @Email) and (SubUsuario.Senha = @Senha) and (Usuario.IdStatusUsuario = 1)
+		where (SubUsuario.Email = @Email) and (SubUsuario.Senha = @Senha)
 	end
