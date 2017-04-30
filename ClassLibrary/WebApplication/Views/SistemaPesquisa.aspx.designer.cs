79ef057c746bf6a62a9650b88ab93e0513305e36
Cleit0nAlex <cleiton.alex@hotmail.com.br>
2017-04-30 14:09:59 -0300

    Adicionado mensagem na tela de pesquisa

--- ClassLibrary/WebApplication/Views/SistemaPesquisa.aspx.designer.cs
+++ ClassLibrary/WebApplication/Views/SistemaPesquisa.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace WebApplication {
     public partial class WebForm4 {
         
         /// <summary>
+        /// divMsg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divMsg;
+        
+        /// <summary>
+        /// msgPesquisa control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label msgPesquisa;
+        
+        /// <summary>
         /// lbItens control.
         /// </summary>
         /// <remarks>
