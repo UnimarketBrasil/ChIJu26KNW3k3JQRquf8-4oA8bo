03aa9e59236865af4cb4d37fcb7d2bacdb131b61
Lucas Sotero <Lucassdeabreu@outlook.com>
2017-05-06 22:43:17 -0300

    pré carrinho

--- ClassLibrary/WebApplication/Views/SistemaCarrinho.aspx.cs
+++ ClassLibrary/WebApplication/Views/SistemaCarrinho.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,21 @@ namespace WebApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Usuario u = (Usuario)Session["sistema"];
+            List<Item> lst = null;
 
+            if (Session["carrinho"] == null)
+            {
+                lst = new List<Item>();
+                Session["carrinho"] = lst;
+            }
+            else
+            {
+                lst = (List<Item>)Session["carrinho"];
+            }
+
+            lbItens.DataSource = lst;
+            lbItens.DataBind();
         }
     }
 }
