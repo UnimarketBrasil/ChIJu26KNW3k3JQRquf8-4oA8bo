970b08640b76884352e2a4251800edf18337cdff
Lucas Sotero <Lucassdeabreu@outlook.com>
2017-06-22 01:28:50 -0300

    nada

--- ClassLibrary/WebApplication/Documentos/Querys/Funcao CalculoDistancia.sql
+++ ClassLibrary/WebApplication/Documentos/Querys/Funcao CalculoDistancia.sql
@@ -3,39 +3,56 @@ go
 if exists ( select name FROM sys.objects where (type in ('FN')) and (name = 'CalculoDistancia')) drop function CalculoDistancia
 go
 create function CalculoDistancia(
-	 @LatitudeVendedor float,
-	 @LongitudeVendedor float,
-	 @LatitudeComprador float,
-	 @LongitudeComprador float
+	 @LatV varchar(20),
+	 @LongV varchar(20),
+	 @LatC varchar(20),
+	 @LongC varchar(20)
 	 )
 returns float
 as
+	
 begin
-declare @Retorno int
-	if(abs(@LatitudeComprador - @LatitudeVendedor) > abs(@LongitudeComprador - @LongitudeVendedor))
-	begin
-		if(@LatitudeComprador - @LatitudeVendedor > 0 and @LongitudeComprador - @LongitudeVendedor > 0)
-		 	set @Retorno = (abs(@LatitudeComprador - @LatitudeVendedor) / cos(atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
-		if(@LatitudeComprador - @LatitudeVendedor > 0 and @LongitudeComprador - @LongitudeVendedor < 0)
-		 	set @Retorno = (abs(@LatitudeComprador - @LatitudeVendedor) / cos(180 - atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
-		if(@LatitudeComprador - @LatitudeVendedor < 0 and @LongitudeComprador - @LongitudeVendedor < 0)
-			set @Retorno =  (abs(@LatitudeComprador - @LatitudeVendedor) / cos(180 + atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
-		if(@LatitudeComprador - @LatitudeVendedor < 0 and @LongitudeComprador - @LongitudeVendedor > 0)
-		 	set @Retorno = (abs(@LatitudeComprador - @LatitudeVendedor) / cos(360 - atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
-	end
-	else
-		if(@LatitudeComprador - @LatitudeVendedor > 0 and @LongitudeComprador - @LongitudeVendedor > 0)
-			set @Retorno = (abs(@LongitudeComprador - @LongitudeVendedor) / cos(atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
-		if(@LatitudeComprador - @LatitudeVendedor > 0 and @LongitudeComprador - @LongitudeVendedor < 0)
-		 	set @Retorno = (abs(@LongitudeComprador - @LongitudeVendedor) / cos(180 - atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
-		if(@LatitudeComprador - @LatitudeVendedor < 0 and @LongitudeComprador - @LongitudeVendedor < 0)
-		 	set @Retorno = (abs(@LongitudeComprador - @LongitudeVendedor) / cos(180 + atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
-		if(@LatitudeComprador - @LatitudeVendedor < 0 and @LongitudeComprador - @LongitudeVendedor > 0)
-		 	set @Retorno = (abs(@LongitudeComprador - @LongitudeVendedor) / cos(360 - atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
-	return @Retorno / 10000
+	declare @Retorno float
+	declare @LatitudeVendedor float
+	declare @LongitudeVendedor float
+	declare @LatitudeComprador float
+	declare @LongitudeComprador float
+	set @LatV = replace(@LatV,'.','')
+	set @LatV = replace(@LatV,',','.')
+	set @LongV = replace(@LongV,'.','')
+	set @LongV = replace(@LongV,',','.')
+	set @LatC = replace(@LatC,'.','')
+	set @LatC = replace(@LatC,',','.')
+	set @LongC = replace(@LongC,'.','')
+	set @LongC = replace(@LongC,',','.')	
+	set @LatitudeVendedor = convert(float, @LatV)
+	set @LongitudeVendedor = convert(float, @LongV)
+	set @LatitudeComprador = convert(float, @LatC)
+	set @LongitudeComprador = convert(float, @LongC)
+	--if(abs(@LatitudeComprador - @LatitudeVendedor) > abs(@LongitudeComprador - @LongitudeVendedor))
+	--begin
+	--	if(@LatitudeComprador - @LatitudeVendedor > 0 and @LongitudeComprador - @LongitudeVendedor > 0)
+	--	 	set @Retorno = (abs(@LatitudeComprador - @LatitudeVendedor) / cos(atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
+	--	if(@LatitudeComprador - @LatitudeVendedor > 0 and @LongitudeComprador - @LongitudeVendedor < 0)
+	--	 	set @Retorno = (abs(@LatitudeComprador - @LatitudeVendedor) / cos(180 - atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
+	--	if(@LatitudeComprador - @LatitudeVendedor < 0 and @LongitudeComprador - @LongitudeVendedor < 0)
+	--		set @Retorno =  (abs(@LatitudeComprador - @LatitudeVendedor) / cos(180 + atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
+	--	if(@LatitudeComprador - @LatitudeVendedor < 0 and @LongitudeComprador - @LongitudeVendedor > 0)
+	--	 	set @Retorno = (abs(@LatitudeComprador - @LatitudeVendedor) / cos(360 - atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
+	--end
+	--else
+	--	if(@LatitudeComprador - @LatitudeVendedor > 0 and @LongitudeComprador - @LongitudeVendedor > 0)
+	--		set @Retorno = (abs(@LongitudeComprador - @LongitudeVendedor) / cos(atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
+	--	if(@LatitudeComprador - @LatitudeVendedor > 0 and @LongitudeComprador - @LongitudeVendedor < 0)
+	--	 	set @Retorno = (abs(@LongitudeComprador - @LongitudeVendedor) / cos(180 - atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
+	--	if(@LatitudeComprador - @LatitudeVendedor < 0 and @LongitudeComprador - @LongitudeVendedor < 0)
+	--	 	set @Retorno = (abs(@LongitudeComprador - @LongitudeVendedor) / cos(180 + atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
+	--	if(@LatitudeComprador - @LatitudeVendedor < 0 and @LongitudeComprador - @LongitudeVendedor > 0)
+	--	 	set @Retorno = (abs(@LongitudeComprador - @LongitudeVendedor) / cos(360 - atan(abs(@LatitudeComprador - @LatitudeVendedor)/abs(@LongitudeComprador - @LongitudeVendedor))) )
+	--return @Retorno / 10000
+	return (sqrt(power(@LatitudeVendedor - @LatitudeComprador,2) + power(@LongitudeVendedor - @LongitudeComprador,2)) /100)
 end		
 		----return ( sqrt( exp((convert(float, @LatitudeComprador)) - (convert(float, @LatitudeVendedor))) + exp((convert(float, @LongitudeComprador)) - (convert(float, @LongitudeVendedor)))))
 ----return sqrt(square(@LatitudeVendedor - @LatitudeComprador) + square(@LongitudeVendedor - @LongitudeComprador))
---return convert(varchar, sqrt(square(abs(@LatitudeVendedor) - abs(@LatitudeComprador)) + square(abs(@LongitudeVendedor) - abs(@LongitudeComprador))) )
 --end
 
--- ClassLibrary/WebApplication/Documentos/Querys/Selects/Procedure MecanismoDeBusca.sql
+++ ClassLibrary/WebApplication/Documentos/Querys/Selects/Procedure MecanismoDeBusca.sql
@@ -6,7 +6,7 @@ if OBJECT_ID('MecanismoDeBuscaCategoria') is not null
 drop procedure MecanismoDeBuscaCategoria
 go
 create procedure MecanismoDeBusca(
-@Pesquisa varchar(50),
+	@Pesquisa varchar(50),
 	@IdComprador int,
 	@LatitudeComprador float,
 	@LongitudeComprador float
@@ -16,6 +16,7 @@ as begin
 	Usuario.Id as IdVendendor from Item
 	inner join Usuario on Usuario.Id = Item.IdUsuario
 	where((dbo.CalculoDistancia( Usuario.Latitude, Usuario.Longitude, @LatitudeComprador, @LongitudeComprador) < Usuario.AreaAtuacao) and (Item.Desabilitado = 'false') and (Item.Quantidade > 0) and (Item.Nome like '%'+@Pesquisa+'%') and (Usuario.Id <> @IdComprador))
+	
 end
 go
 create procedure MecanismoDeBuscaCategoria(
